7ec3a9aea6f9d60eba82c0956f1f779952cecf4e
consolexin <consolexin@gmail.com>
2023-12-10 00:45:39 +0800

    prototype

---
+++ prototype/prototype.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+class ProtoType {
+public:
+    virtual ~ProtoType() {
+        cout << "Destruct()" << endl;
+    }
+
+    virtual ProtoType* Clone() = 0;
+
+protected:
+    ProtoType() = default;
+};
+
+
+class ConcreteProtoType: public ProtoType {
+public:
+    ConcreteProtoType() = default;
+
+    ConcreteProtoType(const ConcreteProtoType& cp) {
+        *this = cp;
+        cout << "ConcretePrototype copy..." << endl;
+    }
+    ~ConcreteProtoType() {
+        cout << "Destruct" << endl;
+    }
+
+    ProtoType* Clone() override {
+        return new ConcreteProtoType(*this);
+    }
+};
+
+int main() {
+    ProtoType* p = new ConcreteProtoType();
+    ProtoType* p1 = p->Clone();
+    ProtoType* p2 = p->Clone();
+    delete p;
+    delete p1;
+    delete p2;
+}
